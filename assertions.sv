845617689c5eca48574cefc8f249aeb6b6229f0e
Mohamed Ahmed Hamed <104694689+mohamedyakoub@users.noreply.github.com>
2025-04-03 13:52:32 +0200

    Update assertions.sv

--- assertions.sv
+++ assertions.sv
@@ -6,27 +6,67 @@ module alu_assertions (
     input [1:0] b_op,
     input signed [5:0] C
 );
+    sequence xor_b;
+	(a_en & b_en) ##0 (b_op==0);
+    endsequence
+    sequence xor_a;
+	(a_en & !b_en) ##0 (a_op==2);
+    endsequence
+    sequence xnor_b;
+	(a_en & b_en) ##0 (b_op==1);
+    endsequence
+    sequence xnor_a;
+	(a_en & !b_en) ##0 (a_op==6);
+    endsequence
+    sequence add_b;
+	(!a_en & b_en) ##0 (b_op==1 || b_op==2 );
+    endsequence
+    sequence add_a;
+	(a_en & !b_en) ##0 (a_op==0);
+    endsequence
 
-    property p1;
-        @(posedge clk) disable iff(!ALU_en || !rst_n) (a_en & b_en) |-> (b_op==0) |=>(C[4:0]==$past(A)^$past(B)) ;
+    property xor_p;
+        @(posedge clk) disable iff(!ALU_en || !rst_n) (xor_a or xor_b) |=>(C[4:0]==$past(A)^$past(B)) ;
     endproperty
-    property p2;
-        @(posedge clk) disable iff(!ALU_en || !rst_n) (a_en & b_en) |-> (b_op==1) |=>(C[4:0]==$past(A)~^$past(B)) ;
+    property xnor_p;
+        @(posedge clk) disable iff(!ALU_en || !rst_n) (xnor_a or xnor_b) |=>(C[4:0]==$past(A)~^$past(B)) ;
     endproperty
-    property p3;
+    property sub_a_p;
         @(posedge clk) disable iff(!ALU_en || !rst_n) (a_en & b_en) |-> (b_op==2) |=>(C==$past(A)-1) ;
     endproperty
-    property p4;
+    property add_b_p;
         @(posedge clk) disable iff(!ALU_en || !rst_n) (a_en & b_en) |-> (b_op==3) |=>(C==$past(B)+2) ;
     endproperty
-    // Assertion for addition
-    assert property (p1);
-    assert property (p2);
-    assert property (p3);
-    assert property (p4);
 
-    cover property  (p1);
-    cover property  (p2);
-    cover property  (p3);
-    cover property  (p4);
+    property add_p;
+        @(posedge clk) disable iff(!ALU_en || !rst_n) (add_a or add_b) |=>(C==$past(A)+$past(B)) ;
+    endproperty
+    property and_p;
+        @(posedge clk) disable iff(!ALU_en || !rst_n) (a_en & !b_en) |-> (a_op== 3 || a_op== 4)  |=> (C[4:0]==($past(A)&$past(B)))  ;
+    endproperty
+    property or_p;
+        @(posedge clk) disable iff(!ALU_en || !rst_n) (a_en & !b_en) |-> (a_op== 5)  |=> (C[4:0]==$past(A)||$past(B)) ;
+    endproperty
+    property nand_p;
+        @(posedge clk) disable iff(!ALU_en || !rst_n) (!a_en & b_en) |-> (b_op== 0)  |=> (C[4:0]==~($past(A)&$past(B))) ;
+    endproperty
+
+    // Assertion for addition
+    xor_assert: assert property (xor_p);
+    xnor_assert: assert property (xnor_p);
+    sub_a_assert: assert property (sub_a_p);
+    add_b_assert: assert property (add_b_p);
+    add_assert: assert property (add_p);
+    and_assert: assert property (and_p);
+    or_assert: assert property (or_p);
+    nand_assert: assert property (nand_p);
+
+    xor_cover: cover property  (xor_p);
+    xnor_cover: cover property  (xnor_p);
+    sub_a_cover:cover property  (sub_a_p);
+    add_b_cover:cover property  (add_b_p);
+    add_cover:cover property  (add_p);
+    and_cover:cover property (and_p);
+    or_cover:cover property (or_p);
+    nand_cover:cover property (nand_p);
 endmodule
